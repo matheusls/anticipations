de42d3caf5a6415aa7eefaf721d1dfcfb55c1090
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-03-30 18:06:37 -0300

    test: create useNavigatorOnline hook

---
+++ src/hooks/useNavigatorOnline/useNavigatorOnline.test.tsx
@@ -0,0 +1,31 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useNavigatorOnline from './useNavigatorOnline';
+
+describe('useNavigatorOnline', () => {
+  const navigatorMock = jest.spyOn(navigator, 'onLine', 'get');
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return true if navigator.onLine is true', () => {
+    navigatorMock.mockReturnValue(true);
+
+    const {
+      result: { current },
+    } = renderHook(() => useNavigatorOnline());
+
+    expect(current).toBe(true);
+  });
+
+  it('should return false if navigator.onLine is false', () => {
+    navigatorMock.mockReturnValue(false);
+
+    const {
+      result: { current },
+    } = renderHook(() => useNavigatorOnline());
+
+    expect(current).toBe(false);
+  });
+});
