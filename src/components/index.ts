671d97e85ac707d5d96b9eaf22895fc2787a938e
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-03-30 00:00:36 -0300

    feat: create LoadingDots component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { default as FormGroup } from './form-group/form-group';
 export { default as Heading } from './heading/heading';
 export { default as Input } from './input/input';
 export { default as Label } from './label/label';
+export { default as LoadingDots } from './loading-dots/loading-dots';
 export { default as TextHelp } from './text-help/text-help';
---
+++ src/components/loading-dots/loading-dots.tsx
@@ -0,0 +1,11 @@
+import { Dot, LoadingDotsStyled } from './loading-dots.styles';
+
+const LoadingDots = () => (
+  <LoadingDotsStyled>
+    <Dot />
+    <Dot />
+    <Dot />
+  </LoadingDotsStyled>
+);
+
+export default LoadingDots;
