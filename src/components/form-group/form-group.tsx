ed3122a20a9c1c90b95a53c0c2ed027542482212
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-03-29 23:47:54 -0300

    feat: create FormGroup component

---
+++ src/components/form-group/form-group.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const FormGroup = styled.div`
+  &:not(:last-of-type) {
+    margin-bottom: 26px;
+  }
+`;
+
+export default FormGroup;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as FormGroup } from './form-group/form-group';
 export { default as Heading } from './heading/heading';
 export { default as Input } from './input/input';
 export { default as Label } from './label/label';
