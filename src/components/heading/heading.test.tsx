376dc5e19ee557710c9e41e3b3b50d127b57d609
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-03-29 23:24:58 -0300

    test: create Heading component

---
+++ src/components/heading/heading.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from 'tests/utils';
+
+import Heading, { HeadingLevels } from './heading';
+
+import { Theme } from 'styles';
+
+const { colors, fontSizes } = Theme;
+
+const defaultStyles = {
+  color: colors.grayDark,
+  fontSize: fontSizes.large,
+  fontStyle: 'normal',
+};
+
+const headingLevels: HeadingLevels[][] = [[1], [2], [3], [4], [5], [6]];
+
+describe('<Heading />', () => {
+  it('should match snapshot', () => {
+    const { container } = render(
+      <Heading level={1}>Heading Component</Heading>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it.each(headingLevels)(
+    '%# should render Heading level %p correctly',
+    (level) => {
+      render(<Heading level={level}>Heading Component</Heading>);
+
+      expect(screen.getByRole('heading', { level })).toBeInTheDocument();
+    },
+  );
+
+  it('should render Heading with correct text', () => {
+    render(<Heading level={1}>Heading Component</Heading>);
+
+    expect(
+      screen.getByRole('heading', { name: /heading component/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render Heading with default styles', () => {
+    render(<Heading level={1}>Heading Component</Heading>);
+
+    const heading = screen.getByRole('heading', { name: /heading component/i });
+
+    const { color, fontSize, fontStyle } = defaultStyles;
+
+    expect(heading).toHaveStyleRule('color', color);
+    expect(heading).toHaveStyleRule('font-size', fontSize);
+    expect(heading).toHaveStyleRule('font-style', fontStyle);
+  });
+});
