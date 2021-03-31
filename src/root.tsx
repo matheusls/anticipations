ee039ac2c5c1d3fb00cc408da8fe3fc2e6167368
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-03-31 01:25:19 -0300

    feat: show toast when user is offline

--- src/root.tsx
+++ src/root.tsx
@@ -1,13 +1,29 @@
+import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import { useNavigatorOnline } from 'hooks';
 import { Home } from 'pages';
 import { GlobalStyles, Theme } from 'styles';
+import { renderErrorToast } from 'utils';
 
-const Root = () => (
-  <ThemeProvider theme={Theme}>
-    <GlobalStyles />
-    <Home />
-  </ThemeProvider>
-);
+const Root = () => {
+  const isOnline = useNavigatorOnline();
+
+  useEffect(() => {
+    if (!isOnline) {
+      renderErrorToast('offline');
+    }
+  }, [isOnline]);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      <ToastContainer />
+      <Home />
+    </ThemeProvider>
+  );
+};
 
 export default Root;
