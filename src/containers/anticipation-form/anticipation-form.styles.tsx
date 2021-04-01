9efb9f80a1e9abc98add34b60fab199027af14eb
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-04-01 17:16:28 -0300

    feat: remove AnticipationForm box-shadow

--- src/containers/anticipation-form/anticipation-form.styles.tsx
+++ src/containers/anticipation-form/anticipation-form.styles.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const AnticipationFormStyled = styled.div`
   border-radius: 4px;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.25);
   display: grid;
   grid-template-columns: 100%;
   overflow: hidden;
